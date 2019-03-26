f619a49f0385702a6b3b07de9fcbe443f96b0d5b
Lingyu Chen <linhchen@Linhs-MacBook-Pro.local>
2019-03-26 03:33:51 -0400

    isSubsequence

---
+++ Java/isSubsequence.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public boolean isSubsequence(String s, String t) {
+
+        if (s.length() == 0)  
+            return true;
+
+        int IndexS = 0;
+        int IndexT = 0;
+        
+        while(IndexT < t.length()) {
+
+            if (t.charAt(IndexT) == s.charAt(IndexS)) {
+                IndexS++;
+
+            // when IndexS equals the len of fomer string, break the loop
+            if (IndexS == s.length())  
+                return true;
+
+            }
+            // tell first, and then IndexT++
+            IndexT++;
+        }
+
+        return false;
+        
+    }
+}
