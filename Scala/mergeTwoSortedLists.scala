c80e86b68438114a364ebfefc67032ae1d8c2e16
Lingyu Chen <linhchen@Linhs-MacBook-Pro.local>
2019-03-28 01:30:46 -0400

    scala merge two lists

---
+++ Scala/mergeTwoSortedLists.scala
@@ -0,0 +1,17 @@
+object Solution{
+	def mergeTwoLists(l1: ListNode, l2: ListNode): ListNode = (l1, l2) match{
+		case (null, null) => null
+		case (a, b) if a != null && b == null => a
+		case (a, b) if a == null && b != null => b
+		case (a, b) if a != null && b != null => {
+			val d = a._x - b._x
+			if(d <= 0) {
+				a.next = mergeTwoLists(a.next, b)
+				a
+			}else {
+				b.next = mergeTwoLists(a, b.next)
+				b
+			}
+		}
+	}
+}
