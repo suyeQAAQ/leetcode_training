ac4641e1157fe44a7a0fdfd27f36c5f9d7cb569b
Lingyu Chen <linhchen@Linhs-MacBook-Pro.local>
2019-03-28 00:58:00 -0400

    two solutions and slight change

---
+++ Java/MaximumSubarray.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        return maxSubArraySum(nums, 0, nums.length - 1);
+    }
+
+    public int maxSumBasedOnMid(int[] nums, int l, int mid, int r) {
+
+        // calculate the left part max sum of subarrays ending with nums[mid]
+        int sum = 0;
+        int leftSum = Integer.MIN_VALUE;
+        for (int i = mid; i >= l; i--) {
+            sum += nums[i];
+            if (sum > leftSum) {
+                leftSum = sum;
+            }
+        }
+        // calculate the right part max sum of subarrays starting with nums[mid + 1]
+        sum = 0;
+        int rightSum = Integer.MIN_VALUE;
+        for (int i = mid + 1; i <= r; i++) {
+            sum += nums[i];
+            if (sum > rightSum) {
+                rightSum = sum;
+            }
+        }
+
+        return leftSum + rightSum;
+    }
+
+    public int maxSubArraySum(int[] nums, int l, int r) {
+        if (l == r) return nums[l];
+        int mid = l + (r - l) / 2;
+        return maxOfThree(maxSubArraySum(nums, l, mid),
+                maxSubArraySum(nums, mid + 1, r),
+                maxSumBasedOnMid(nums, l, mid, r));
+    }
+
+    public int maxOfThree(int a, int b, int c) {
+        return Math.max(a, Math.max(b, c));
+    }
+}
+    
---
+++ Java/mergeTwoSortedLists.java
@@ -0,0 +1,32 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
+
+        while (l1 != null || l2 != null) {
+            if (l1 != null && l2 != null) {
+
+                if (l1.val <= l2.val) {
+                    temp.next = new ListNode(l1.val);
+                    temp = temp.next;
+                    l1 = l1.next;
+                } else {
+                    temp.next = new ListNode(l2.val);
+                    temp = temp.next;
+                    l2 = l2.next;
+                }
+            } else {
+                if (l1 == null) {
+                    temp.next = l2;
+                    break;
+                }
+                if (l2 == null) {
+                    temp.next = l1;
+                    break;
+                }
+            }
+        }
+
+        return head.next;
+    }
+}
